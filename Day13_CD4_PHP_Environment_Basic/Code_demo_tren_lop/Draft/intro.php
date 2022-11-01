7a78a50ce283a57c27c43f0169262dfaeee65159
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2022-11-01 09:54:40 +0700

    update

---
+++ Day13_CD4_PHP_Environment_Basic/Code_demo_tren_lop/Draft/intro.php
@@ -0,0 +1,17 @@
+<!--C:/xampp/htdocs/php0722e_test/intro.php-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Intro</title>
+        <style>
+            h1 {color: red;}
+        </style>
+    </head>
+    <body>
+        <?php
+        // PHP giấu đc mã nguồn, còn HTML CSS JS thì ko
+        echo 'manhnv';
+        ?>
+        <h1 onclick="alert('Click h1')">Hello World</h1>
+    </body>
+</html>
---
+++ Day13_CD4_PHP_Environment_Basic/Code_demo_tren_lop/Draft/test.php
@@ -0,0 +1,14 @@
+<?php
+// Cách chạy code PHP:
+// - Source bắt buộc phải đặt trong htdocs
+// - Chạy url sau: localhost tham chiếu đến thư mục htdocs
+//của xampp
+// http://localhost/php0722_test/test.php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+echo date('d-m-Y H:i:s');
+// - Cách chạy file trực tiếp bằng PHPStorm mà ko cần quan
+// tâm đến đường dẫn file, điều kiện bắt buộc là phải dùng
+//PHPStorm mở thư mục gốc chứa file php cần chạy
+// C:/xampp/htdocs/a/b/c/d/e/f/g/h/i/test.php
+
+?>
