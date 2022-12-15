dd4256ef937398d7db40585059b8d2c29ce335c6
Manhnv <nguyenvietmanhit@gmail.com>
2022-12-15 21:14:38 +0700

    update

---
+++ Day24_PDO_MVC_Demo_Website/Codes_demo/mvc/controllers/UserController.php
@@ -0,0 +1,8 @@
+<?php
+//controllers/UserController.php
+
+class UserController {
+    public function create() {
+        echo 'THêm mới user';
+    }
+}
--- Day24_PDO_MVC_Demo_Website/Codes_demo/pdo.php
+++ Day24_PDO_MVC_Demo_Website/Codes_demo/pdo.php
@@ -37,4 +37,62 @@ $inserts = [
 //B4: Thực obj truy vấn: INSERT, UPDATE, DELETE trả về boolean
 $is_insert = $obj_insert->execute($inserts);
 var_dump($is_insert);
-var_dump($is_insert);
+
+// 2 - UPDATE: cập nhật tên tuổi cho user có id = 1
+// B1: Viết truy vấn dạng tham số:
+$sql_update = "UPDATE users SET name=:name,age=:age WHERE id=:id";
+// B2: Cbi obj truy vấn:
+$obj_update = $connection->prepare($sql_update);
+// B3: Tạo mảng truyền giá trị cho tham số truy vấn:
+$updates = [
+    ':name' => 'manhnv update',
+    ':age' => 10,
+    ':id' => 1
+];
+// B4: Thực thi: UPDATE trả về boolean
+$is_update = $obj_update->execute($updates);
+var_dump($is_update);
+// 3 - DELETE: xóa user có id > 5
+// B1: Viết truy vấn dạng tham số:
+$sql_delete = "DELETE FROM users WHERE id > :id";
+// B2: Cbi obj truy vấn:
+$obj_delete = $connection->prepare($sql_delete);
+// B3: Tạo mảng:
+$deletes = [
+    ':id' => 5
+];
+// B4: Thực thi: DELETE trả về boolean
+$is_delete = $obj_delete->execute($deletes);
+var_dump($is_delete);
+// 4 - SELECT
+// - Lấy 1 bản ghi: lấy user có id = 1
+// B1: Viết truy vấn:
+$sql_select_one = "SELECT * FROM users WHERE id = :id";
+// B2: Cbi
+$obj_select_one = $connection->prepare($sql_select_one);
+// B3: Tạo mảng
+$selects = [
+    ':id' => 1
+];
+// B4: Thực thi, SELECT ko cần qtam đến kiểu dữ liệu trả về
+$obj_select_one->execute($selects);
+// B5: Trả về mảng kết hợp 1 chiều:
+$user = $obj_select_one->fetch(PDO::FETCH_ASSOC);
+echo '<pre>';
+print_r($user);
+echo '</pre>';
+
+// - Lấy nhiều bản ghi: lấy tất cả user
+// B1: Viết truy vấn:
+$sql_select_all = "SELECT * FROM users";
+// B2: Cbi:
+$obj_select_all = $connection->prepare($sql_select_all);
+// B3: Bỏ qua vì câu truy vấn ko có tham số
+// B4: Thực thi
+$obj_select_all->execute();
+// B5: Trả về mảng kết hợp 2 chiều:
+$users = $obj_select_all->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+print_r($users);
+echo '</pre>';
+
